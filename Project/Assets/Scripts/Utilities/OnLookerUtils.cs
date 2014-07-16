2cc5a91236d37fa07da7949cdb00667818e2d68b
nathanhanlan <nathanhanlan@gmail.com>
2014-07-16 15:47:08 -0400

    Commit 1.0.0
    Changes:
    • Added & implemented OnLookerUtils
    • Added & implemented UIEventArgs / UIEventType

---
+++ Project/Assets/Scripts/Utilities/OnLookerUtils.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace OnLooker
+{
+    public enum MouseButton
+    {
+        LEFT,
+        RIGHT,
+        MIDDLE,
+        NONE
+    }
+
+
+    [ExecuteInEditMode()]
+    public class OnLookerUtils
+    {
+        public static bool anyMouseButtonDown()
+        {
+            if (Input.GetMouseButtonDown((int)MouseButton.LEFT))
+            {
+                return true;
+            }
+            if (Input.GetMouseButtonDown((int)MouseButton.MIDDLE))
+            {
+                return true;
+            }
+            if (Input.GetMouseButtonDown((int)MouseButton.RIGHT))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool pointInRect(Vector2 aPoint, Rect aRect)
+        {
+            return pointInRect(aPoint, aRect, true);
+        }
+        public static bool pointInRect(Vector2 aPoint, Rect aRect, bool yUp)
+        {
+            //Is the Y Direction Up or down
+            if (yUp == true)
+            {
+                if (aPoint.x < aRect.x || aPoint.y < aRect.y || aPoint.x > aRect.x + aRect.width || aPoint.y > aRect.y + aRect.height)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (aPoint.x < aRect.x || aPoint.y > aRect.y + aRect.height || aPoint.x > aRect.x + aRect.width || aPoint.y < aRect.y)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static float clampAngle(float angle, float min, float max)
+        {
+            if (angle < -360.0f)
+            {
+                angle += 360.0f;
+            }
+            else if (angle > 360.0f)
+            {
+                angle -= 360.0f;
+            }
+            return Mathf.Clamp(angle, min, max);
+        }
+
+        
+        
+    }
+}
