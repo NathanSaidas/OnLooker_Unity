81773a80939984c7e828860cd141cdd58c9806d3
NathanSaidas <NathanSaidas@users.noreply.github.com>
2014-10-31 01:48:17 -0400

    Changes:
    ::Utilities::
    • Added methods for parsing words from a string.
    
    ::DebugUtils::
    • Added compilation conditional log helpers.
    • Added and implemented a in game console. Toggleable through F3

---
+++ Project/Assets/Scripts/Utilities/Debug/ConsoleMessage.cs
@@ -0,0 +1,40 @@
+﻿namespace Gem
+{
+    public enum LogLevel
+    {
+        LOG,
+        WARNING,
+        ERROR,
+        USER
+    }
+
+    public struct ConsoleMessage
+    {
+        private string m_Message;
+        private LogLevel m_LogLevel;
+
+        
+       
+        public ConsoleMessage(string aMessage)
+        {
+            m_Message = aMessage;
+            m_LogLevel = LogLevel.LOG;
+        }
+        public ConsoleMessage(string aMessage, LogLevel aLogLevel)
+        {
+            m_Message = aMessage;
+            m_LogLevel = aLogLevel;
+        }
+
+        public string message
+        {
+            get { return m_Message; }
+            set { m_Message = value; }
+        }
+        public LogLevel logLevel
+        {
+            get { return m_LogLevel; }
+            set { m_LogLevel = value; }
+        }
+    }
+}
--- Project/Assets/Scripts/Utilities/Utilities.cs
+++ Project/Assets/Scripts/Utilities/Utilities.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-
+using System.Collections.Generic;
+#region CHANGE LOG
+/* October,30,2014 - Nathan Hanlan, Added a ParseToWords method.
+ * 
+ */
+#endregion
 
 public static class Utilities
 {
@@ -13,7 +17,7 @@ public static class Utilities
     /// <param name="aMin">The minimum value of the angle.</param>
     /// <param name="aMax">The maximum value of the angle.</param>
     /// <returns></returns>
-    public static float clampAngle(float aAngle, float aMin, float aMax)
+    public static float ClampAngle(float aAngle, float aMin, float aMax)
     {
         if (aAngle < -360)
             aAngle += 360;
@@ -31,7 +35,7 @@ public static class Utilities
     /// <param name="aEnd">The target value.</param>
     /// <param name="aSpeed">The speed to move at. (This is clamped to 0 to 1 value).</param>
     /// <returns></returns>
-    public static float exponentialEase(float aStart, float aEnd, float aSpeed)
+    public static float ExponentialEase(float aStart, float aEnd, float aSpeed)
     {
         float diff = aEnd - aStart;
 
@@ -47,12 +51,146 @@ public static class Utilities
     /// <param name="aEnd">The target value.</param>
     /// <param name="aSpeed">The speed to move at. (This is clamped to 0 to 1 value).</param>
     /// <returns></returns>
-    public static Vector3 exponentialEase(Vector3 aStart, Vector3 aEnd, float aSpeed)
+    public static Vector3 ExponentialEase(Vector3 aStart, Vector3 aEnd, float aSpeed)
     {
         Vector3 diff = aEnd - aStart;
 
         diff *= Mathf.Clamp(aSpeed, 0.0f, 1.0f);
 
         return diff + aStart;
+    }
+    /// <summary>
+    /// Parses a string for all of its words, uses a space character as a separator.
+    /// </summary>
+    /// <param name="aContext">The string to parse</param>
+    /// <returns>A list of words from the context. </returns>
+    public static List<string> ParseToWords(string aContext, bool aToLower)
+    {
+        List<string> words = new List<string>();
+        if (aContext.Length == 0)
+        {
+            return words;
+        }
+        if(aToLower == true)
+        {
+            aContext = aContext.ToLower();
+        }
+        if (aContext.Length == 1)
+        {
+            words.Add(aContext);
+            return words;
+        }
+        int startIndex = -1;
+        int endIndex = 0;
+        int spaceIndex = 0;
+
+        while (aContext.Length > 0)
+        {
+            startIndex = -1;
+            endIndex = 0;
+            spaceIndex = 0;
+            if (aContext.Length == 1)
+            {
+                words.Add(aContext);
+                break;
+            }
+
+            for (int i = 0; i < aContext.Length; i++)
+            {
+                if (startIndex == -1 && aContext[i] != ' ')
+                {
+                    startIndex = i;
+                    continue;
+                }
+                if (startIndex != -1 && aContext[i] == ' ')
+                {
+                    spaceIndex = i;
+                    break;
+                }
+                if (i == aContext.Length - 1)
+                {
+                    spaceIndex = aContext.Length;
+                    break;
+                }
+            }
+
+            endIndex = spaceIndex - 1;
+            int wordLength = Mathf.Clamp(endIndex - startIndex + 1, 0, aContext.Length);
+            int removeLength = Mathf.Clamp(spaceIndex - startIndex + 1, 0, aContext.Length);
+            string word = aContext.Substring(startIndex, wordLength);
+            aContext = aContext.Remove(startIndex, removeLength);
+            words.Add(word);
+        }
+
+
+        return words;
+    }
+
+    /// <summary>
+    /// Parses a string for all of its words using the specified separtor.
+    /// </summary>
+    /// <param name="aContext">The string to parse</param>
+    /// <param name="aSeparator">The value which determines a space in the sentence</param>
+    /// <returns>A list of words from the context. </returns>
+    public static List<string> ParseToWords(string aContext,char aSeparator, bool aToLower)
+    {
+        List<string> words = new List<string>();
+        if (aContext.Length == 0)
+        {
+            return words;
+        }
+        if(aToLower == true)
+        {
+            aContext = aContext.ToLower();
+        }
+        if (aContext.Length == 1)
+        {
+            words.Add(aContext);
+            return words;
+        }
+        int startIndex = -1;
+        int endIndex = 0;
+        int spaceIndex = 0;
+
+        while (aContext.Length > 0)
+        {
+            startIndex = -1;
+            endIndex = 0;
+            spaceIndex = 0;
+            if (aContext.Length == 1)
+            {
+                words.Add(aContext);
+                break;
+            }
+
+            for (int i = 0; i < aContext.Length; i++)
+            {
+                if (startIndex == -1 && aContext[i] != aSeparator)
+                {
+                    startIndex = i;
+                    continue;
+                }
+                if (startIndex != -1 && aContext[i] == aSeparator)
+                {
+                    spaceIndex = i;
+                    break;
+                }
+                if (i == aContext.Length - 1)
+                {
+                    spaceIndex = aContext.Length;
+                    break;
+                }
+            }
+
+            endIndex = spaceIndex - 1;
+            int wordLength = Mathf.Clamp(endIndex - startIndex + 1, 0, aContext.Length);
+            int removeLength = Mathf.Clamp(spaceIndex - startIndex + 1, 0, aContext.Length);
+            string word = aContext.Substring(startIndex, wordLength);
+            aContext = aContext.Remove(startIndex, removeLength);
+            words.Add(word);
+        }
+
+
+        return words;
     }
 }
